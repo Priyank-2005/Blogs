7f8c5109d1857b95b91c5cfc8c445d4efff0612e
Priyank-2005 <priyank.bohra.31@gmail.com>
2025-06-13 12:41:52 +0530

    Fix1

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,16 +2,24 @@ import { notFound } from 'next/navigation';
 import posts from '../../data/posts.json';
 import '../../../styles/Post.css';
 
-type Params = {
+type PostPageProps = {
   params: {
     slug: string;
   };
 };
 
-export default function PostPage({ params }: Params) {
-  const post = posts.find(p => p.slug === params.slug);
+export async function generateStaticParams() {
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+
+export default function PostPage({ params }: PostPageProps) {
+  const post = posts.find((p) => p.slug === params.slug);
 
   if (!post) return notFound();
+
   return (
     <div className="post-container">
       <h1>{post.title}</h1>
